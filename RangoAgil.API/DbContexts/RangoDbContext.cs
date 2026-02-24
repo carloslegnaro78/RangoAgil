075b6bbe1b5a6ebabed34bec457788a08d771185
carlos.legnaro <carlos.legnaro@outlook.com>
2026-02-24 20:10:37 -0300

    EF Core

--- RangoAgil.API/DbContexts/RangoDbContext.cs
+++ RangoAgil.API/DbContexts/RangoDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace RangoAgil.API.DbContexts;
+﻿using Microsoft.EntityFrameworkCore;
+using RangoAgil.API.Entities;
 
-public class RangoDbContext
+namespace RangoAgil.API.DbContexts;
+
+public class RangoDbContext(DbContextOptions<RangoDbContext> options) : DbContext(options)
 {
-    public DbSet MyProperty { get; set; }
-}
+    public DbSet<Rango> Rangos { get; set; } = null!;
+    public DbSet<Ingrediente> Ingredientes { get; set; } = null!;   
 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
