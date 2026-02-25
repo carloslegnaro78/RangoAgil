5b7d90e418181d14b04c33b3ecbe7202b56cda48
carlos.legnaro <carlos.legnaro@outlook.com>
2026-02-24 21:59:36 -0300

    Explicando e Populando DB

--- RangoAgil.API/Entities/Rango.cs
+++ RangoAgil.API/Entities/Rango.cs
@@ -7,11 +7,13 @@ public class Rango
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     [MaxLength(200)]
     public required string Nome { get; set; }
 
     public ICollection<Ingrediente> Ingredientes { get; set; } = [];
+
     public Rango()
     {
 
