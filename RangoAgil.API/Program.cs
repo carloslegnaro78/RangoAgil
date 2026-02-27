2315ab011ea9ae66f1452ce7ec432e9a015c547e
carlos.legnaro <carlos.legnaro@outlook.com>
2026-02-26 23:23:23 -0300

    HTTP Results

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RangoAgil.API.DbContexts;
+using RangoAgil.API.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,15 +14,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/rangos", async (RangoDbContext rangoDbContext, [FromQuery(Name = "name")] string? rangoNome) => {
+app.MapGet("/rangos", async Task<Results<NoContent, Ok<List<Rango>>>> (RangoDbContext rangoDbContext, [FromQuery(Name = "name")] string rangoNome) =>
+{
 
-    return await rangoDbContext.Rangos
+    var rangosEntity = await rangoDbContext.Rangos
         .Where(x => x.Nome.Contains(rangoNome))
         .ToListAsync();
 
+    if (rangosEntity.Count <= 0 || rangosEntity == null)
+        return TypedResults.NoContent();
+    else
+        return TypedResults.Ok(rangosEntity);
+
 });
 
-app.MapGet("/rango/{id:int}", async (RangoDbContext rangoDbContext, int id) => {
+app.MapGet("/rango/{id:int}", async (RangoDbContext rangoDbContext, int id) =>
+{
 
     return await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == id);
 
