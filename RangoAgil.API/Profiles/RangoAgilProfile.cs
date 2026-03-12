4e3a02fdfcd040d16f45f9f7ab7b30f802151a6e
carlos.legnaro <carlos.legnaro@outlook.com>
2026-03-12 16:29:04 -0300

    Endpoint Put

--- RangoAgil.API/Profiles/RangoAgilProfile.cs
+++ RangoAgil.API/Profiles/RangoAgilProfile.cs
@@ -8,6 +8,7 @@ public class RangoAgilProfile : Profile
     {
         CreateMap<Rango, RangoDTO>().ReverseMap();
         CreateMap<Rango, RangoParaCriacaoDTO>().ReverseMap();
+        CreateMap<Rango, RangoParaAtualizacaoDTO>().ReverseMap();
         CreateMap<Ingrediente, IngredienteDTO>()
             .ForMember(
                 d => d.RangoId,
--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -73,4 +73,22 @@ app.MapPost("/rango", async Task<CreatedAtRoute<RangoDTO>> (
     //     rangoToReturn);
 });
 
+app.MapPut("/rango/{id:int}", async Task<Results<NotFound, Ok>> (
+    RangoDbContext rangoDbContext,
+    IMapper mapper,
+    int id,
+    [FromBody] RangoParaAtualizacaoDTO rangoParaAtualizacaoDTO) =>
+{
+    var rangosEntity = await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == id);
+    if (rangosEntity == null)
+        return TypedResults.NotFound();
+
+    mapper.Map(rangoParaAtualizacaoDTO, rangosEntity);
+
+    await rangoDbContext.SaveChangesAsync();
+
+    return TypedResults.Ok();
+});
+
+
 app.Run();
