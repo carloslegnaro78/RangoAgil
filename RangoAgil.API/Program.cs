bebc34fb55244f980bf0ed3ec9eaff9c43416b28
carlos.legnaro <carlos.legnaro@outlook.com>
2026-03-12 16:44:36 -0300

    Endpoint Delete

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -90,5 +90,19 @@ app.MapPut("/rango/{id:int}", async Task<Results<NotFound, Ok>> (
     return TypedResults.Ok();
 });
 
+app.MapDelete("/rango/{id:int}", async Task<Results<NotFound, NoContent>> (
+    RangoDbContext rangoDbContext,
+    int id) =>
+{
+    var rangosEntity = await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == id);
+    if (rangosEntity == null)
+        return TypedResults.NotFound();
+
+    rangoDbContext.Rangos.Remove(rangosEntity);
+
+    await rangoDbContext.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+});
 
 app.Run();
